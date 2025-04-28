4e834aeefef18c3037c19cd43c46060aacfe63c0
Luna Skye <sepshuncontact@gmail.com>
2025-04-28 03:39:36 -0400

    style(tests): whitespace in let/in block

--- tests/default.nix
+++ tests/default.nix
@@ -3,6 +3,7 @@
 let
   colors_test = import ./colors_test.nix { inherit pkgs helpers; };
   math_test = import ./math_test.nix { inherit pkgs helpers; };
+
 in {
   colors = colors_test;
   math = math_test;
