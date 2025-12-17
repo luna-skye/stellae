ed4a469f87bd858ad01fa62383b2bbe78d303be2
Luna Skye <sepshuncontact@gmail.com>
2025-12-16 23:52:13 -0500

    refactor(flake): use core nixpkgs

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
 
 
   outputs = { self, nixpkgs }: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    pkgs = nixpkgs;
     helpers = import ./helpers { inherit pkgs; };
 
     rawElements = helpers.getElements ./elements;
