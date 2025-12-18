f44596e95796ae129d988c9fd764022c7c6707a6
Luna Skye <sepshuncontact@gmail.com>
2025-12-18 18:33:04 -0500

    fix(exporters/gowall): incorrect json formatting

--- exporters/gowall/default.nix
+++ exporters/gowall/default.nix
@@ -6,29 +6,29 @@
   {
     "name": "stellae-${element.name}",
     "colors": [
-      "${surface.crust}"
-      "${surface.mantle}"
-      "${surface.base}"
-      "${surface.surface0}"
-      "${surface.surface1}"
-      "${surface.overlay0}"
-      "${surface.overlay1}"
-      "${surface.subtext0}"
-      "${surface.subtext1}"
-      "${surface.text}"
-      "${accent.red}"
-      "${accent.light_red}"
-      "${accent.orange}"
-      "${accent.light_orange}"
-      "${accent.yellow}"
-      "${accent.light_yellow}"
-      "${accent.green}"
-      "${accent.light_green}"
-      "${accent.blue}"
-      "${accent.light_blue}"
-      "${accent.purple}"
-      "${accent.light_purple}"
-      "${accent.magenta}"
+      "${surface.crust}",
+      "${surface.mantle}",
+      "${surface.base}",
+      "${surface.surface0}",
+      "${surface.surface1}",
+      "${surface.overlay0}",
+      "${surface.overlay1}",
+      "${surface.subtext0}",
+      "${surface.subtext1}",
+      "${surface.text}",
+      "${accent.red}",
+      "${accent.light_red}",
+      "${accent.orange}",
+      "${accent.light_orange}",
+      "${accent.yellow}",
+      "${accent.light_yellow}",
+      "${accent.green}",
+      "${accent.light_green}",
+      "${accent.blue}",
+      "${accent.light_blue}",
+      "${accent.purple}",
+      "${accent.light_purple}",
+      "${accent.magenta}",
       "${accent.light_magenta}"
     ]
   }
